2ab25682743c8b084e73dd3544c78ef0f8e351d9
Melkiy <Melkiy@Melkiy-PC>
2019-05-26 09:07:52 +0200

    Version 1.0 version base

---
+++ proyecto_prueba_examED/src/proyecto_prueba_examED/clase_prueba_exam_ED.java
@@ -0,0 +1,11 @@
+package proyecto_prueba_examED;
+
+public class clase_prueba_exam_ED {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola exam");
+
+	}
+
+}
